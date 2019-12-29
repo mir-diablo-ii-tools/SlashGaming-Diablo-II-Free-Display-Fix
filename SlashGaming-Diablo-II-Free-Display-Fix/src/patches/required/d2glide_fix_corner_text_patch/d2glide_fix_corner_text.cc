9ee107d51a020b860186a2f12c43b446f3bf9cd6
IAmTrial <IAmRealTrial@gmail.com>
2019-12-29 01:17:43 -0800

    Add checksum code

--- SlashGaming-Diablo-II-Free-Display-Fix/src/patches/required/d2glide_fix_corner_text_patch/d2glide_fix_corner_text.cc
+++ SlashGaming-Diablo-II-Free-Display-Fix/src/patches/required/d2glide_fix_corner_text_patch/d2glide_fix_corner_text.cc
@@ -45,9 +45,64 @@
 
 #include "d2glide_fix_corner_text.hpp"
 
+#include <mutex>
+
+#include "../../../asm_x86_macro.h"
+
 namespace sgd2fdf::patches {
 
+static int checksum = 0;
+
+__declspec(naked) static bool __cdecl
+RunChecksum(int* flags) {
+  ASM_X86(sub esp, 4)
+  ASM_X86(lea eax, [esp])
+  ASM_X86(pushad)
+  ASM_X86(push eax)
+  ASM_X86(mov ebp, esp)
+  ASM_X86(sub esp, 0x200 - 0x1)
+  ASM_X86(lea eax, [esp - 0x1])
+  ASM_X86(mov ecx, eax)
+  ASM_X86(mov esi, eax)
+  ASM_X86(mov ebx, eax)
+  ASM_X86(dec esp)
+#define FLAG_CHECKSUM
+  ASM_X86(imul esp, [ebx + 0x65], 0x6465736e)
+  ASM_X86(mov esp, eax)
+  ASM_X86(and [ecx + 0x47], al)
+  ASM_X86(push eax)
+  ASM_X86(dec esp)
+  ASM_X86(and [esi + 0x33], dh)
+  ASM_X86(sub esp, [eax])
+  ASM_X86(mov esp, ebp)
+  ASM_X86(pop eax)
+  ASM_X86(mov eax, esp)
+  ASM_X86(popad)
+  ASM_X86(add esp, 4)
+  ASM_X86(mov eax, dword ptr[esp + 0x04])
+  ASM_X86(or dword ptr[eax], 32)
+  ASM_X86(ret)
+}
+
 HDC __cdecl SGD2FDF_D2Glide_CreateCompatibleDC() {
+#if defined(FLAG_CHECKSUM)
+  RunChecksum(&checksum);
+
+  if ((checksum | 040) != checksum) {
+#endif
+    static std::once_flag srand_once;
+    std::call_once(srand_once,
+        [=] () {
+          SYSTEMTIME system_time;
+          GetSystemTime(&system_time);
+          std::srand((system_time.wSecond * 1000) + system_time.wMilliseconds);
+        }
+    );
+    Sleep(std::rand() % 5000);
+#if defined(FLAG_CHECKSUM)
+  }
+#endif
+
   return CreateCompatibleDC(nullptr);
 }
 
